f33e477371d2de6842eb2fe34eec638cf6ad2861
Sebastian Stumpf <sebastian.stumpf87@googlemail.com>
2013-06-02 19:00:26 +0200

    Groups are now displayed as panels, so drag and drop can be applied.

--- src/main/java/edu/hm/cs/ivaacal/MyVaadinUI.java
+++ src/main/java/edu/hm/cs/ivaacal/MyVaadinUI.java
@@ -1,11 +1,16 @@
 package edu.hm.cs.ivaacal;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.event.dd.DragAndDropEvent;
+import com.vaadin.event.dd.DropHandler;
+import com.vaadin.event.dd.acceptcriteria.AcceptAll;
+import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
+import edu.hm.cs.ivaacal.controller.EphemeralUserController;
 import edu.hm.cs.ivaacal.controller.PersistentUserController;
 import edu.hm.cs.ivaacal.dataSource.GooglePlusSource;
 import edu.hm.cs.ivaacal.exception.DataSourceException;
@@ -15,11 +20,17 @@ import edu.hm.cs.ivaacal.model.User;
 import edu.hm.cs.ivaacal.model.Worker;
 import edu.hm.cs.ivaacal.model.transport.GroupTO;
 import edu.hm.cs.ivaacal.model.transport.UserTO;
+import fi.jasoft.dragdroplayouts.DDAccordion;
+import fi.jasoft.dragdroplayouts.DDHorizontalLayout;
+import fi.jasoft.dragdroplayouts.client.ui.LayoutDragMode;
+import fi.jasoft.dragdroplayouts.drophandlers.DefaultAccordionDropHandler;
 import org.apache.commons.configuration.Configuration;
 import org.apache.log4j.Logger;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 
 
@@ -39,9 +50,8 @@ public class MyVaadinUI extends UI {
 
 
     // TODO: *************WORKER*************
-    private GridLayout generateWorker(Worker worker) {
+    private Component generateWorker(Worker worker) {
 
-        // TODO: build layout -> worker field
         GridLayout grid = new GridLayout();
         grid.addStyleName("worker");
         grid.setRows(2);
@@ -54,31 +64,31 @@ public class MyVaadinUI extends UI {
         grid.addComponent(new Label(worker.getName(), ContentMode.HTML), 1, 0);
 
         // Fill in Availability
-        // dummy start
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Label availability = new Label("available until:<br />" + dateFormat.format(new Date()), ContentMode.HTML);
-        availability.addStyleName("available");
-        // dummy end
-        /*if(worker.isAvailable()) {
-            availability =  new Label("available until:<br />"+worker.getAvailabilityChangeDate(), ContentMode.HTML);
+        long availableMinutes = (worker.getAvailabilityChangeDate().getTime() - new Date().getTime())/60000;
+        long availableHours = availableMinutes/60;
+        availableMinutes %= 60;
+        Label availability;
+        if(worker.isAvailable() != null && worker.isAvailable()) {
+            availability =  new Label("available for:<br />" + (availableHours == 0l? "": availableHours + " hours, ") + availableMinutes + " minutes", ContentMode.HTML);
             availability.addStyleName("available");
         }
         else {
-            availability =  new Label("available again at:<br />"+worker.getAvailabilityChangeDate(), ContentMode.HTML);
+            availability =  new Label("available in:<br />" + (availableHours == 0l? "": availableHours + " hours, ") + availableMinutes + " minutes", ContentMode.HTML);
             availability.addStyleName("unavailable");
-        }*/
+        }
         grid.addComponent(availability, 0, 1, 1, 1);
 
         // TODO: add function -> drag and drop
-        // TODO: add function -> initialize from user / default user object
+        DragAndDropWrapper draggableWorkerWrapper = new DragAndDropWrapper(grid);
+        draggableWorkerWrapper.setDragStartMode(DragAndDropWrapper.DragStartMode.WRAPPER);
 
-        return grid;
+        return draggableWorkerWrapper;
     }
 
 
     // TODO: *************LOGIN*************
-    private HorizontalLayout generateLoginField() {
-        // TODO: build layout -> logged out fields
+    private Component generateLoginField() {
+
         HorizontalLayout login = new HorizontalLayout();
         login.addStyleName("login");
         TextField username = new TextField();
@@ -95,85 +105,67 @@ public class MyVaadinUI extends UI {
         return login;
     }
 
-    private HorizontalLayout generateHeader() {
+    private Component generateHeader() {
 
+        // The header Component
         HorizontalLayout header = new HorizontalLayout();
         header.setWidth("100%");
-        HorizontalLayout login = generateLoginField();
+
+        // Add Login Fields
+        Component login = generateLoginField();
         header.addComponent(login);
+
+        // Add Headline
         Label title = new Label("iVaaCal");
         title.addStyleName("h2");
         header.addComponent(title);
+
         header.setComponentAlignment(title, Alignment.MIDDLE_RIGHT);
         header.setComponentAlignment(login, Alignment.MIDDLE_LEFT);
 
         return header;
     }
 
-    // TODO: *************GROUPS*************
-    private Accordion generateGroups(UserTO user) throws ModifyUserException {
+    private Component generateGroup(GroupTO group){
 
-        // TODO: build layout -> groups accordion
-        Accordion groups = new Accordion();
-        groups.setHeight(100.0f, Unit.PERCENTAGE);
+        // The group component
+        VerticalLayout groupLayout = new VerticalLayout();
+        groupLayout.setMargin(true);
 
-        // sample for generating a second group
-        try {
-            Worker member = new GooglePlusSource().loadWorker("111163807910558864867");
-            final HorizontalLayout members = new HorizontalLayout();
-            members.addComponent(generateWorker(member));
-            members.setMargin(false);
-            groups.addTab(members, "It's a me, Mario");
-        } catch (DataSourceException e) {
-            e.printStackTrace();
-        }
-        try {
-            Worker member = new GooglePlusSource().loadWorker("111163807910558864867");
-            final HorizontalLayout members = new HorizontalLayout();
-            members.addComponent(generateWorker(member));
-            members.setMargin(false);
-            groups.addTab(members, "It's a me, Lol");
-        } catch (DataSourceException e) {
-            e.printStackTrace();
-        }
-        try {
-            Worker member = new GooglePlusSource().loadWorker("111163807910558864867");
-            final HorizontalLayout members = new HorizontalLayout();
-            members.addComponent(generateWorker(member));
-            members.setMargin(false);
-            groups.addTab(members, "It's a me, Hubert");
-        } catch (DataSourceException e) {
-            e.printStackTrace();
-        }
-        try {
-            Worker member = new GooglePlusSource().loadWorker("111163807910558864867");
-            final HorizontalLayout members = new HorizontalLayout();
-            members.addComponent(generateWorker(member));
-            members.setMargin(false);
-            groups.addTab(members, "It's a me, Hansi");
-        } catch (DataSourceException e) {
-            e.printStackTrace();
-        }
-        // end sample
+        // The name of the group
+        Panel panel = new Panel(group.getName());
+        groupLayout.addComponent(panel);
 
-        // TODO: add function -> initialize from user / default user object
-        for (GroupTO group : user.getGroups()) {
-            final HorizontalLayout members = new HorizontalLayout();
-            for (Worker member : group.getWorkers()) {
-                members.addComponent(generateWorker(member));
-            }
-            members.setMargin(false);
-            groups.addTab(members, group.getName());
+        // The members of the group
+        HorizontalLayout workersLayout = new HorizontalLayout();
+        for (Worker member : group.getWorkers()) {
+            workersLayout.addComponent(generateWorker(member));
         }
+        panel.setContent(workersLayout);
 
         // TODO: add function -> create group
         // TODO: add function -> delete group
-        // TODO: add function -> add worker
+        // TODO: add function -> add worker per drag and drop
         // TODO: add function -> delete worker
 
-        return groups;
+        return groupLayout;
     }
 
+    private Component generateGroups(UserTO user) {
+
+        // The groups component
+        VerticalLayout groupsLayout = new VerticalLayout();
+
+        // Generate an add groups one by one
+        for (GroupTO group : user.getGroups()) {
+            groupsLayout.addComponent(generateGroup(group));
+            groupsLayout.addComponent(generateGroup(group));
+            groupsLayout.addComponent(generateGroup(group));
+        }
+
+        return groupsLayout;
+
+    }
 
     @Override
     protected void init(VaadinRequest request) {
@@ -187,8 +179,6 @@ public class MyVaadinUI extends UI {
         //generate group area
         try {
             root.addComponent(generateGroups(new PersistentUserController("Sebastian Stumpf").getUserTO()));
-        } catch (ModifyUserException e) {
-            e.printStackTrace();
         } catch (DataSourceException e) {
 			e.printStackTrace();
 		}
@@ -199,25 +189,8 @@ public class MyVaadinUI extends UI {
         // TODO: build layout -> show results
 
 
-        // felix testarea
-        /*final TextField googleIdField = new TextField();
-        root.addComponent(googleIdField);
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                try {
-                    Worker worker = new GooglePlusSource().loadWorker(googleIdField.getValue());
-                    VerticalLayout workerLayout = new VerticalLayout();
-                    workerLayout.addComponent(new Label(worker.getName()));
-                    workerLayout.addComponent(new Image(null, new ExternalResource(worker.getImageURL())));
-                    root.addComponent(workerLayout);
-                } catch (DataSourceException e) {
-                    LOGGER.error(e);
-                }
-            }
-        });
-        root.addComponent(button);
-        */
+        // testarea
+
     }
 
 }
